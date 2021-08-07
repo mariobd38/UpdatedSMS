06cfcef017b16a1defb138d6c4a094b940887634
Owner <owner@Owners-Air-2.attlocal.net>
2021-08-06 18:57:19 -0700

    Init

---
+++ src/main/java/App/Practice.java
@@ -0,0 +1,16 @@
+package App;
+
+import java.util.Random;
+
+public class Practice {
+  public static class Random {
+    public void hello() {
+      System.out.println("hello world");
+    }
+  }
+
+  public static void main(String[] args) {
+    Random r = new Random();
+    r.hello();
+  }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module TestProject {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens App;
+}
